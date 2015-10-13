7f6bb9fd1d6f3d620a3fc675f8426f2369d658c1
Magnus Aspling <Yugge@ludd.ltu.se>
2015-10-13 23:23:43 +0200

    Added support for getting strem urls/audio data

--- clients/webclient.go
+++ clients/webclient.go
@@ -1,6 +1,10 @@
 package clients
 
 import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"github.com/antonholmquist/jason"
@@ -28,6 +32,8 @@ var InvalidCredentialsError = errors.New("ERROR: Invalid credentials")
 var NotImplementedError = errors.New("ERROR: Feature not implemented")
 
 var baseUrl = "https://play.google.com/music/"
+
+//var baseUrl = "http://192.168.2.174:6666/"
 var serviceUrl = baseUrl + "services/"
 
 //Generates a new WebClient
@@ -113,12 +119,54 @@ func (w *WebClient) GetSongDownloadInfo() error {
 	return NotImplementedError
 }
 
-func (w *WebClient) GetStreamUrls() error {
-	return NotImplementedError
+func (w *WebClient) GetStreamUrls(id string) (*[]string, error) {
+	params := url.Values{}
+	params.Add("u", "0")
+	params.Add("pt", "e")
+
+	key := "27f7313e-f75d-445a-ac99-56386a5fe879"
+	salt := "sgeoq1k4jm85"
+	mac := hmac.New(sha1.New, []byte(key))
+	mac.Write([]byte(id + salt))
+	hash := mac.Sum(nil)
+	sig := base64.URLEncoding.EncodeToString(hash)
+	params.Add("slt", salt)
+	params.Add("sig", sig)
+	if id[0] == 'T' {
+		params.Add("mjck", id)
+	} else {
+		params.Add("songid", id)
+	}
+
+	browser := surf.NewBrowser()
+	urlen, _ := url.Parse("https://play.google.com")
+	cookieJar := jar.NewMemoryCookies()
+	cookieJar.SetCookies(urlen, w.Session.Cookies)
+	browser.SetCookieJar(cookieJar)
+
+	browser.Open(baseUrl + "play?" + params.Encode())
+	payload := html.UnescapeString(browser.Body())
+	js, _ := jason.NewObjectFromReader(strings.NewReader(payload))
+	urls, _ := js.GetStringArray("urls")
+	return &urls, nil
 }
 
-func (w *WebClient) GetStreamAudio() error {
-	return NotImplementedError
+func (w *WebClient) GetStreamAudio(id string) (*[]byte, error) {
+	browser := surf.NewBrowser()
+	urlen, _ := url.Parse("https://play.google.com")
+	cookieJar := jar.NewMemoryCookies()
+	cookieJar.SetCookies(urlen, w.Session.Cookies)
+	browser.SetCookieJar(cookieJar)
+
+	urls, _ := w.GetStreamUrls(id)
+	b := bytes.NewBuffer(make([]byte, 20000000))
+	for i, v := range *urls {
+		w.Logger.Info("Parsing url %v/%v", i+1, len(*urls))
+		browser.Open(v)
+		browser.Download(b)
+	}
+	audioData := b.Bytes()
+	return &audioData, nil
 }
 
 func (w *WebClient) ReportIncorrectMatch() error {
