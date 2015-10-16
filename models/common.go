f8b07b9f595026b443a462ff1c4c534b68eec632
Magnus Aspling <yugge@ludd.ltu.se>
2015-10-16 02:31:37 +0200

    Mmmh. midnight coding

--- models/common.go
+++ models/common.go
@@ -54,7 +54,7 @@ func NewPlaylist(data []*jason.Value) (p *Playlist) {
 }
 
 func newSong(data []*jason.Value) (s Song) {
-	s.Id, err := data[0].String()
+	s.Id, err = data[0].String()
 	if err != nil {
 		s.Id = ""
 	}
